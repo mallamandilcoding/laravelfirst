c2f044a697a73bc017da822af8f5c27cf69f4c0e
Mandil <mallamandilcoding@gmail.com>
2023-10-07 14:26:05 +0900

    edit profile image js done when selecting image auto displaying it in iamge section

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -32,14 +32,15 @@
                             <div class="row mb-3">
                                 <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
                                 <div class="col-sm-10">
-                                    <input class="form-control" placeholder="Email" id="example-text-input" type="file">
+                                    <input name="profile_image" class="form-control" placeholder="" id="image"
+                                        type="file">
                                 </div>
                             </div>
 
                             <div class="row mb-3">
                                 <label for="example-text-input" class="col-sm-2 col-form-label"></label>
                                 <div class="col-sm-10">
-                                    <img class="rounded avatar-lg"
+                                    <img id="showImage" class="rounded avatar-lg"
                                         src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
                                 </div>
                             </div>
@@ -56,5 +57,31 @@
     </div>
 
 </div>
+
+<script>
+    var fileInput = document.getElementById("image");
+    var selectedImage = document.getElementById("showImage");
+
+    fileInput.addEventListener("change", function(event) {
+        var file = event.target.files[0]; // Get the selected file
+        if (file) {
+            var reader = new FileReader(); // Create a new FileReader object
+
+            // Event listener for when the FileReader has successfully read the file
+            reader.onload = function(e) {
+            // Update the src attribute of the image with the data URL of the selected file
+            selectedImage.src = e.target.result;
+            };
+
+            // Read the selected file as a data URL
+            reader.readAsDataURL(file);
+        } else {
+            // If no file was selected, you can handle it here (e.g., display an error message)
+            console.log("No file selected.");
+        }
+    });
+
+
+</script>
 
 @endsection
