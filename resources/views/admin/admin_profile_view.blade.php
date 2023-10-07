90944199c360d9b39dfaad5ca049a91c381aaae0
Mandil <mallamandilcoding@gmail.com>
2023-10-07 12:23:01 +0900

    admin page done

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -1,12 +1,32 @@
 @extends('admin.admin_master')
 @section('admin')
 
-    <div class="page-content">
-        <div class="container-fluid">
+<div class="page-content">
+    <div class="container-fluid">
 
+        <div class="row justify-content-center">
+            <div class="col-6">
 
+                <div class="card-group">
+
+                    <div class="card">
+                        <img class="img-thumbnail rounded-circle avatar-xl mx-auto"
+                            src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">Name: {{ $adminData->name }}</h4>
+                            <hr>
+                            <h4 class="card-title">Email: {{ $adminData->email }}</h4>
+                            <hr>
+                            <a href="#" class="btn btn-primary btn-rounded waves-effect waves-light">Edit
+                                Profile</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
-
     </div>
+
+</div>
 
 @endsection
